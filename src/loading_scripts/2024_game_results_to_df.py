9e58f15167a93103a5254b76790b38a1705787af
Aaron Morey <aaronmorey@gmail.com>
2025-06-04 00:36:48 -0500

    Initial pass of pythag vs actual wins

--- src/loading_scripts/2024_game_results_to_df.py
+++ src/loading_scripts/2024_game_results_to_df.py
@@ -2,8 +2,47 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 
+shortcode_to_team = {
+    "ARI": "Arizona Diamondbacks",
+    "ATL": "Atlanta Braves",
+    "BAL": "Baltimore Orioles",
+    "BOS": "Boston Red Sox",
+    "CHC": "Chicago Cubs",
+    "CHW": "Chicago White Sox",
+    "CIN": "Cincinnati Reds",
+    "CLE": "Cleveland Guardians",
+    "COL": "Colorado Rockies",
+    "DET": "Detroit Tigers",
+    "HOU": "Houston Astros",
+    "KCR": "Kansas City Royals",
+    "LAA": "Los Angeles Angels",
+    "LAD": "Los Angeles Dodgers",
+    "MIA": "Miami Marlins",
+    "MIL": "Milwaukee Brewers",
+    "MIN": "Minnesota Twins",
+    "NYM": "New York Mets",
+    "NYY": "New York Yankees",
+    "OAK": "Oakland Athletics",
+    "PHI": "Philadelphia Phillies",
+    "PIT": "Pittsburgh Pirates",
+    "SDP": "San Diego Padres",
+    "SEA": "Seattle Mariners",
+    "SFG": "San Francisco Giants",
+    "STL": "St. Louis Cardinals",
+    "TBR": "Tampa Bay Rays",
+    "TEX": "Texas Rangers",
+    "TOR": "Toronto Blue Jays",
+    "WSN": "Washington Nationals"
+}
+
+team_to_shortcode = {v: k for k, v in shortcode_to_team.items()}
+
 def parse_team_from_anchor(anchor):
     return anchor['href'].split('/')[2]
+
+# Formula per Wikipedia: https://en.wikipedia.org/wiki/Pythagorean_expectation
+def pythag_wins(runs_scored, runs_allowed, games_played):
+    return 1 / (1 + ((runs_allowed / runs_scored) ** 1.83)) * games_played
 
 # I copied this straight from the source of https://www.baseball-reference.com/leagues/majors/2024-schedule.shtml
 with open('data/2024_game_results.html') as file:
@@ -13,6 +52,7 @@ games = soup.find_all('p', class_='game')
 
 run_differentials = {}
 skeleton = {
+            'games': 0,
             'rs': 0,
             'ra': 0,
             'diff': 0,
@@ -35,12 +75,14 @@ for g in games:
     home_differential = home_score - away_score
 
     if not home_team in run_differentials:
-        run_differentials[home_team] = skeleton
+        run_differentials[home_team] = skeleton.copy()
 
     if not away_team in run_differentials:
-        run_differentials[away_team] = skeleton
+        run_differentials[away_team] = skeleton.copy()
 
     # print(f'{home_team} {home_score} - {away_team} {away_score}')
+    run_differentials[home_team]['games'] += 1
+    run_differentials[away_team]['games'] += 1
     run_differentials[home_team]['rs'] = run_differentials[home_team]['rs'] + home_score
     run_differentials[home_team]['ra'] = run_differentials[home_team]['ra'] + away_score
     run_differentials[away_team]['rs'] = run_differentials[away_team]['rs'] + away_score
@@ -49,10 +91,28 @@ for g in games:
     # run_differentials[away_team]['diff_blowout4'] = run_differentials[away_team]['diff_blowout4'] + max(4,away_score - home_score)
     # run_differentials[home_team]['diff_blowout5'] = run_differentials[home_team]['diff_blowout4'] + max(5,home_score - away_score)
     # run_differentials[away_team]['diff_blowout5'] = run_differentials[away_team]['diff_blowout4'] + max(5,away_score - home_score)
-    if home_team == 'LAD':
-        print(f'{i} {run_differentials['LAD']}')
-        i+=1
+    # if home_team == 'LAD':
+    #     print(f'{i} {run_differentials['LAD']}')
+    #     i+=1
+
+for team_id, team_stats in run_differentials.items():
+    # print(team_id)
+    run_differentials[team_id]['pythag'] = pythag_wins(team_stats['rs'], team_stats['ra'], team_stats['games'])
+
+# CITE: https://www.baseball-reference.com/leagues/majors/2024-standings.shtml
+actual_team_results = pd.read_csv('data/2024_team_results.csv')
+
+for row in actual_team_results.itertuples():
+    print(row)
+    # Don't process the last "Average" row
+    if row.Tm == 'Average':
+        continue
+    team_shortcode = team_to_shortcode[row.Tm]
+    # print(team_shortcode)
+    rd = run_differentials[team_shortcode]
+    rd['actual_wins'] = row.W
 
 # print(run_differentials)
-# df = pd.DataFrame(run_differentials)
-# print(df.T.to_string())
+df = pd.DataFrame.from_dict(run_differentials, orient='index')
+print(df)
+print(df[['pythag', 'actual_wins']].corr())
