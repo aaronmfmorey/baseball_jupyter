655d4c547bb03dead44eb658d65acf3582462f2b
Aaron Morey <aaronmorey@gmail.com>
2025-06-05 12:53:32 -0500

    Add blowout calculations and copy to Jupyter

--- src/loading_scripts/2024_game_results_to_df.py
+++ src/loading_scripts/2024_game_results_to_df.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 
+
 shortcode_to_team = {
     "ARI": "Arizona Diamondbacks",
     "ATL": "Atlanta Braves",
@@ -44,6 +45,25 @@ def parse_team_from_anchor(anchor):
 def pythag_wins(runs_scored, runs_allowed, games_played):
     return 1 / (1 + ((runs_allowed / runs_scored) ** 1.83)) * games_played
 
+# Sample home_score = 13, away_score = 2, max_diff = 5
+def limited_run_differential(home_score, away_score, max_diff):
+    """
+        Limit the run differential of the game to reduce the impact of blowouts.
+        I've chosen to reduce the winning team's score and leaving the losing team's
+        score the same.
+    """
+    actual_diff = abs(home_score - away_score)
+    if actual_diff > max_diff:
+        if home_score > away_score:
+            home_score -= (actual_diff - max_diff)
+        else:
+            away_score -= (actual_diff - max_diff)
+
+    return {
+        "home_score": home_score,
+        "away_score": away_score
+    }
+
 # I copied this straight from the source of https://www.baseball-reference.com/leagues/majors/2024-schedule.shtml
 with open('data/2024_game_results.html') as file:
     soup = BeautifulSoup(file, features='html.parser')
@@ -59,7 +79,9 @@ skeleton = {
             'rs_blowout4': 0,
             'ra_blowout4': 0,
             'ra_blowout5': 0,
-            'rs_blowout5': 0
+            'rs_blowout5': 0,
+            'ra_blowout6': 0,
+            'rs_blowout6': 0
         }
 
 i = 1
@@ -80,24 +102,37 @@ for g in games:
     if not away_team in run_differentials:
         run_differentials[away_team] = skeleton.copy()
 
-    # print(f'{home_team} {home_score} - {away_team} {away_score}')
     run_differentials[home_team]['games'] += 1
     run_differentials[away_team]['games'] += 1
     run_differentials[home_team]['rs'] = run_differentials[home_team]['rs'] + home_score
     run_differentials[home_team]['ra'] = run_differentials[home_team]['ra'] + away_score
     run_differentials[away_team]['rs'] = run_differentials[away_team]['rs'] + away_score
     run_differentials[away_team]['ra'] = run_differentials[away_team]['ra'] + home_score
-    # run_differentials[home_team]['diff_blowout4'] = run_differentials[home_team]['diff_blowout4'] + max(4,home_score - away_score)
-    # run_differentials[away_team]['diff_blowout4'] = run_differentials[away_team]['diff_blowout4'] + max(4,away_score - home_score)
-    # run_differentials[home_team]['diff_blowout5'] = run_differentials[home_team]['diff_blowout4'] + max(5,home_score - away_score)
-    # run_differentials[away_team]['diff_blowout5'] = run_differentials[away_team]['diff_blowout4'] + max(5,away_score - home_score)
-    # if home_team == 'LAD':
-    #     print(f'{i} {run_differentials['LAD']}')
-    #     i+=1
+
+    blowout4 = limited_run_differential(home_score, away_score, 4)
+    run_differentials[home_team]['rs_blowout4'] = run_differentials[home_team]['rs_blowout4'] + blowout4['home_score']
+    run_differentials[home_team]['ra_blowout4'] = run_differentials[home_team]['ra_blowout4'] + blowout4['away_score']
+    run_differentials[away_team]['rs_blowout4'] = run_differentials[away_team]['rs_blowout4'] + blowout4['away_score']
+    run_differentials[away_team]['ra_blowout4'] = run_differentials[away_team]['ra_blowout4'] + blowout4['home_score']
+
+    blowout5 = limited_run_differential(home_score, away_score, 5)
+    run_differentials[home_team]['rs_blowout5'] = run_differentials[home_team]['rs_blowout5'] + blowout5['home_score']
+    run_differentials[home_team]['ra_blowout5'] = run_differentials[home_team]['ra_blowout5'] + blowout5['away_score']
+    run_differentials[away_team]['rs_blowout5'] = run_differentials[away_team]['rs_blowout5'] + blowout5['away_score']
+    run_differentials[away_team]['ra_blowout5'] = run_differentials[away_team]['ra_blowout5'] + blowout5['home_score']
+
+    blowout6 = limited_run_differential(home_score, away_score, 6)
+    run_differentials[home_team]['rs_blowout6'] = run_differentials[home_team]['rs_blowout6'] + blowout6['home_score']
+    run_differentials[home_team]['ra_blowout6'] = run_differentials[home_team]['ra_blowout6'] + blowout6['away_score']
+    run_differentials[away_team]['rs_blowout6'] = run_differentials[away_team]['rs_blowout6'] + blowout6['away_score']
+    run_differentials[away_team]['ra_blowout6'] = run_differentials[away_team]['ra_blowout6'] + blowout6['home_score']
 
 for team_id, team_stats in run_differentials.items():
     # print(team_id)
     run_differentials[team_id]['pythag'] = pythag_wins(team_stats['rs'], team_stats['ra'], team_stats['games'])
+    run_differentials[team_id]['pythag4'] = pythag_wins(team_stats['rs_blowout4'], team_stats['ra_blowout4'], team_stats['games'])
+    run_differentials[team_id]['pythag5'] = pythag_wins(team_stats['rs_blowout5'], team_stats['ra_blowout5'], team_stats['games'])
+    run_differentials[team_id]['pythag6'] = pythag_wins(team_stats['rs_blowout6'], team_stats['ra_blowout6'], team_stats['games'])
 
 # CITE: https://www.baseball-reference.com/leagues/majors/2024-standings.shtml
 actual_team_results = pd.read_csv('data/2024_team_results.csv')
@@ -116,3 +151,6 @@ for row in actual_team_results.itertuples():
 df = pd.DataFrame.from_dict(run_differentials, orient='index')
 print(df)
 print(df[['pythag', 'actual_wins']].corr())
+print(df[['pythag4', 'actual_wins']].corr())
+print(df[['pythag5', 'actual_wins']].corr())
+print(df[['pythag6', 'actual_wins']].corr())
